b75af067c6ded9f33ff41bc509e113d73bb7b153
lcliao <lcliao@nurlink.com>
2020-09-08 16:11:55 +0800

    freq_loop sv2 sv5 done

--- CorherentSum.m
+++ CorherentSum.m
@@ -1,19 +1,19 @@
-function [Sum_I,Sum_Q] = CorherentSum(Input_I,Input_Q,CAcode1023,CarrierNcoStep,CodeNcoStep,CarrierNcoPhase...
-    ,CodeNcoPhase,Ms)
+function [Sum_I,Sum_Q,CodeNcoPhase,CarrierNcoPhase,CodeIdx] = CorherentSum(Input_I,Input_Q,CAcode1023,CarrierNcoStep,CodeNcoStep,CarrierNcoPhase...
+    ,CodeNcoPhase,Ms,sum_I,sum_Q,codeidx)
 %CORHERENTSUM 此处显示有关此函数的摘要
 %   此处显示详细说明
 global sin_table;
 global cos_table;
 
-    Sum_I = 0;
-    Sum_Q = 0;
-    CodeIdx = 0;
+    Sum_I = sum_I;
+    Sum_Q = sum_Q;
+    CodeIdx = codeidx;
     CodeNcoPhaseOld = CodeNcoPhase;
 
     for n=1:1:2048*Ms
         CodeNcoPhase = mod(CodeNcoPhase + CodeNcoStep,2^32-1);
         CarrierNcoPhase = mod(CarrierNcoPhase + CarrierNcoStep,2^32-1);
-        PhaseIdx = bitand(bitshift(CarrierNcoPhase,-28),15)+1;
+        PhaseIdx = bitand(bitshift(uint32(CarrierNcoPhase),-28),15)+1;
         I_temp = Input_I(n) * cos_table(PhaseIdx) - Input_Q(n) * sin_table(PhaseIdx);
         Q_temp = Input_Q(n) * cos_table(PhaseIdx) + Input_I(n) * sin_table(PhaseIdx);
         Sum_I = Sum_I + I_temp * CAcode1023(mod(CodeIdx,1023)+1);
